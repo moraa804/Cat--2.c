5b4d578393b6723266458f7034a288baa4d0e068
moraa804 <nancymoraa207@gmail.com>
2024-11-17 10:18:40 +0300

    Create Even no.c

---
+++ Even no.c
@@ -0,0 +1,25 @@
+/*
+name: Nancy moraa Kebaso 
+reg no:CT101/G/23248/24
+*/
+
+
+#include<stdio.h>
+
+int main(){
+int num;
+int even;
+printf("enter even number:");
+scanf("%d",&num);
+
+if(num %2==0){
+  printf("the number is even");
+  }
+  else
+  {
+  printf("the number is odd");
+  }
+  
+  
+  return 0;
+  }
